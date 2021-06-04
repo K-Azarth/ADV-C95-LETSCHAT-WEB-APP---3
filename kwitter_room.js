94a1e6977abd380dc72485b0e7656197a5fa0102
K-Azarth <74723292+K-Azarth@users.noreply.github.com>
2021-06-04 12:52:18 +0530

    Update kwitter_room.js

--- kwitter_room.js
+++ kwitter_room.js
@@ -16,7 +16,6 @@ function addRoom() {
     room_name = document.getElementById("room_name").value;
     firebase.database().ref("/").child(room_name).update({ purpose: "adding room name" });
     localStorage.setItem("room_name", room_name);
-    window.location = "kwitter_page.html";
 }
 
 function getData() {
